e6f7654bdcf40bfee7fffd9028251c953455513d
Johnny Matthews <johnny.matthews@gmail.com>
2019-04-10 20:54:24 -0500

    Update ip_me.py

--- ip_me.py
+++ ip_me.py
@@ -1,1 +1,20 @@
-#import requests
+#! /usr/bin/env python
+
+
+import requests
+from socket import gethostname, gethostbyname
+
+
+headers = {
+    'cache-control': "no-cache",
+}
+
+payload = gethostname()+" "+gethostbyname(gethostname())
+hook = {"value1": payload,
+        "value2": response.status_code, 
+        "value3": ""}
+
+response = requests.request("POST", 
+                            "https://maker.ifttt.com/trigger/button_triple/with/key/dhh2AN6ZYlKW4o6Bl6ImY0", 
+                            data=hook, 
+                            headers=headers)
